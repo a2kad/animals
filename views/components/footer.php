6cd29a59cac7fb2a871e1ad977bcfd66d9361cb7
a2kad <reshetnikovruslan705@gmail.com>
2023-07-06 14:38:07 +0200

    Ajoute js bootstrap

--- views/components/footer.php
+++ views/components/footer.php
@@ -8,7 +8,7 @@
     </footer>
 </div>
 
-
+<script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,22 +1,18 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+
     <div class="container-fluid">
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarsExample03">
-
                 <ul class="navbar-nav me-auto mb-2 mb-sm-0">
                     <li class="nav-item">
                         <a class="nav-link active" href="../index.php">Accueil</a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link active" href="../controllers/controller-admin.php">Admin</a>
                     </li>
-
-
                 </ul>
                 <form role="search">
                     <input class="form-control" type="search" placeholder="Search" aria-label="Search">
@@ -24,4 +20,5 @@
             </div>
         </div>
     </div>
+
 </nav>
