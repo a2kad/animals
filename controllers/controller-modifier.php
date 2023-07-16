a08671cf5d049206a4164da1747344d512f927c9
a2kad <reshetnikovruslan705@gmail.com>
2023-07-16 23:22:46 +0200

    change modif

--- controllers/controller-modifier.php
+++ controllers/controller-modifier.php
@@ -4,25 +4,37 @@ require_once "../helpers/Database.php";
 require_once "../models/Animals.php";
 require_once "../models/Gerer.php";
 
-if (isset($_POST['submit'])) {
-    $id = $_POST['id'];
-    $date_of_birth = htmlspecialchars($_POST['date_of_birth']);
-    $tatoo = htmlspecialchars($_POST['tatoo']);
-    $chip = htmlspecialchars($_POST['chip']);
-    $name = htmlspecialchars($_POST['name']);
-    $weight = htmlspecialchars($_POST['weight']);
-    $id_color = htmlspecialchars($_POST['id_color']);
-    $id_type = htmlspecialchars($_POST['id_type']);
-    $id_sex = htmlspecialchars($_POST['sex']);
-    $id_race = htmlspecialchars($_POST['id_race']);
+$regexString = '/^[a-zA-Z]+$/';
+$error = [];
 
-    $result_modif = Gerer::modifierAnimal($id, $date_of_birth, $tatoo, $chip, $name, $weight, $id_color, $id_type, $id_sex, $id_race);
-    if($result_modif){
-        echo 'Modif OK';
-    }else{
-        echo 'Modif Error';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["name"])) {
+        $name = htmlspecialchars($_POST['name']);
+
+        if (empty($name)) {
+            $error["name"] = "Champs obligatoire";
+        } else if (!preg_match($regexString, $name)) {
+            $error["name"] = "Mauvais format du nom";
+        }
+    }
+    if (isset($_POST['submit']) && count($error) == 0) {
+        $id = $_POST['id'];
+        $date_of_birth = htmlspecialchars($_POST['date_of_birth']);
+        $tatoo = htmlspecialchars($_POST['tatoo']);
+        $chip = htmlspecialchars($_POST['chip']);
+        
+        $weight = htmlspecialchars($_POST['weight']);
+        $id_color = htmlspecialchars($_POST['id_color']);
+        $id_type = htmlspecialchars($_POST['id_type']);
+        $id_sex = htmlspecialchars($_POST['sex']);
+        $id_race = htmlspecialchars($_POST['id_race']);
+
+        $result_modif = Gerer::modifierAnimal($id, $date_of_birth, $tatoo, $chip, $name, $weight, $id_color, $id_type, $id_sex, $id_race);
+        if ($result_modif) {
+            echo 'Modif OK';
+        } else {
+            echo 'Modif Error';
+        }
     }
 }
-
 include '../views/modifier.php';
-
--- views/modifier.php
+++ views/modifier.php
@@ -12,15 +12,15 @@
                 <fieldset class="m-5">
                     <input type="hidden" class="form-control" name="id" id="id" value="<?= $row['id'] ?>">
                     <div class="mb-5">
-                        <label for="name" class="form-label">Veuillez indiquer le nom de l'animal : <i><?= $error["name"] ?? "" ?></i> </label>
-                        <input type="text" class="form-control mywidth m-auto text-center" name="name" id="name" value="<?= $row['name'] ?>">
+                        <label for="name" class="form-label">Veuillez indiquer le nom de l'animal : <i class="error"><?= $error["name"] ?? "" ?></i> </label>
+                        <input type="text" class="form-control mywidth m-auto text-center" name="name" id="name" value="<?= $_POST['name']??$row['name']  ?>">
                     </div>
                     <div class="mb-5">
-                        <label for="type" class="form-label">Quel type d'animal est-ce ? <i><?= $error["animaltype"] ?? "" ?></i></label>
+                        <label for="type" class="form-label">Quel type d'animal est-ce ? <i class="error"><?= $error["animaltype"] ?? "" ?></i></label>
                         <select class="form-select text-center mywidth m-auto" id="type" name="id_type">
                             <?php
                             foreach (Gerer::getType() as $row_type) { ?>
-                                <option <?= $row['id_type'] == $row_type['id'] ? 'selected' : '' ?> value="<?= $row_type['id'] ?>"><?= $row_type['type'] ?></option>
+                                <option <?= $row['id_type'] == $row_type['id'] ? 'selected' : '' ?> value="<?= $_POST['id_type']??$row_type['id'] ?>"><?= $row_type['type'] ?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -29,7 +29,7 @@
                         <select class="form-select text-center mywidth m-auto" id="name_race" name="id_race">
                             <?php
                             foreach (Gerer::getRace($row['id_type']) as $row_race) { ?>
-                                <option <?= $row['id_race'] == $row_race['id'] ? 'selected' : '' ?> value="<?= $row_race['id'] ?>"><?= $row_race['name_race'] ?></option>
+                                <option <?= $row['id_race'] == $row_race['id'] ? 'selected' : '' ?> value="<?= $_POST['id_race']??$row_race['id'] ?>"><?= $row_race['name_race'] ?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -78,29 +78,25 @@
                     </div>
 
                     
-                    <div class="mb-5">
-                        <label for="chip" class="form-label">Chip</label>
-                        <select class="form-select" id="chip" name="chip">
-                            <option <?= $row['chip'] == 0 ? 'selected' : '' ?> value="0">Non</option>
-                            <option <?= $row['chip'] == 1 ? 'selected' : '' ?> value="1">Oui</option>
-                        </select>
+                    <div class="checkbox mb-5">
+                    <p>L'animal est-il pucé : <i><?= $error["chip"] ?? "" ?></i></p>
+                    <div class="form-check">
+                        <input class="form-check-input mb-3" type="radio" name="chip" id="flexRadioDefault1" value="1" <?= $row['chip'] == 1 ? "checked" : "" ?>>
+                        <label class="form-check-label ms-3" for="flexRadioDefault1">
+                            Oui
+                        </label>
                     </div>
+                    <div class="form-check">
+                        <input class="form-check-input mb-3" type="radio" name="chip" id="flexRadioDefault2" value="0" <?= $row['chip'] == 0 ? "checked" : "" ?>>
+                        <label class="form-check-label ms-3" for="flexRadioDefault2">
+                            Non
+                        </label>
+                    </div>
+                </div>
                     
 
                     <button value="submit" name="submit" type="submit" class="btn btn-primary" id="liveToastBtn">Modifier</button>
-                    <div class="toast-container position-fixed bottom-0 end-0 p-3">
-                        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <img src="..." class="rounded me-2" alt="...">
-                                <strong class="me-auto">Bootstrap</strong>
-                                <small>11 mins ago</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                Hello, world! This is a toast message.
-                            </div>
-                        </div>
-                    </div>
+                    
                 </fieldset>
             </form>
     <?php }
